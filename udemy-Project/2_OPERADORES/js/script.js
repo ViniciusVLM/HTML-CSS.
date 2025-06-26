695a1a0787b018da1a72541505cd40c3b3811c69
Vinizinho-py <vini172006v@gmail.com>
2025-06-25 23:48:25 -0300

    Special numbers

--- udemy-Project/2_OPERADORES/js/script.js
+++ udemy-Project/2_OPERADORES/js/script.js
@@ -3,4 +3,24 @@ console.log(typeof 2);
 console.log(typeof 5.14);
 console.log(typeof -127);
 
+// 2 - OPS . aritimeticas
+
+console.log(2+4);
+console.log(10 - 5);
+console.log( 10 /2);
+console.log(10*10);
+
+
+console.log( 5 + (4 * 2))
+
+// 3 - Special numbers
+
+console.log(typeof Infinity);
+
+console.log(typeof - Infinity);
+
+console.log( 12 * "asd")
+
+console.log(typeof NaN)
+
 
