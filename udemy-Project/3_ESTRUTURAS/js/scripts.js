c71192a722119539fcfc95a61aabef10b1d26e08
Vinizinho-py <vini172006v@gmail.com>
2025-06-29 23:31:30 -0300

    Estruturas if e else

--- udemy-Project/3_ESTRUTURAS/js/scripts.js
+++ udemy-Project/3_ESTRUTURAS/js/scripts.js
@@ -38,11 +38,83 @@ console.log(_teste)
 
 // 3 -  prompt
 
-const age = prompt("Digite a sua idade")
+//const age = prompt("Digite a sua idade")
 
-console.log(`Você tem ${age} anos.`)
+//console.log(`Você tem ${age} anos.`)
 
 // 4 - alert
+
+//alert("Testando");
+
+//const z = 10
+
+//alert(`O número é ${z}`)
+
+// 5 - Math
+
+console.log(Math.max(5, 1, 2, 6 ,4 ,7, 10) ) 
+
+console.log(Math.floor(5, 1, 2, 6 ,4 ,7, 10) ) 
+
+console.log(Math.ceil(5.14) ) 
+
+// 6 - Console
+console.log("Teste!")
+
+console.error("erro!")
+
+console.warn("Aviso!")
+
+
+// 7 - if 
+
+const m = 10
+
+if(m> 5) {
+
+    console.log("M é maior que 5")
+
+}
+
+const user = "João"
+
+if(user === "João"){
+
+    console.log("É João")
+
+}
+
+if(user === "Maria"){
+
+    console.log("Olá Maria")
+
+}
+
+console.log(user === "João", user === "Maria")
+
+// 8 - else 
+
+const loggedIn = false
+
+if(loggedIn) {
+    console.log("Está Autenticado!")
+}else{
+    console.log("Não está autenticado")
+}
+
+
+
+const q = 10
+const w = 25
+
+if(q > 5 && w > 20) {
+    console.log("Numeros mais altos")
+}else{
+    console.log("Os numeros não são mais altos")
+}
+
+
+
 
 
 
