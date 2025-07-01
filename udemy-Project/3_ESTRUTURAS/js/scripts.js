2c669acbf6187a4c92fa420486c323369fb7107e
Vinizinho-py <vini172006v@gmail.com>
2025-06-30 23:40:26 -0300

    While na Pratica

--- udemy-Project/3_ESTRUTURAS/js/scripts.js
+++ udemy-Project/3_ESTRUTURAS/js/scripts.js
@@ -1,141 +1,165 @@
 // 1 - Variaveis
 
-let nome = "Vincius"
+// let nome = "Vincius"
 
 
-console.log(nome)
-console.log("Matheus")
+// console.log(nome)
+// console.log("Matheus")
 
-nome = "Vinicius Lourenço"
+// nome = "Vinicius Lourenço"
 
-console.log(nome)
+// console.log(nome)
 
-const idade = 18
+// const idade = 18
 
-console.log(idade)
+// console.log(idade)
 
-console.log(typeof nome)
+// console.log(typeof nome)
 
-console.log(typeof idade)
+// console.log(typeof idade)
 
-// 2 - mais sobre variaveis
+// // 2 - mais sobre variaveis
 
-let a =10, b = 20, c=30
-console.log(a, b, c)
+// let a =10, b = 20, c=30
+// console.log(a, b, c)
 
-const nomecompleto = "Vinicius Lourenço"
+// const nomecompleto = "Vinicius Lourenço"
 
-const nomeCompleto = "Vinicius Martins"
+// const nomeCompleto = "Vinicius Martins"
 
-console.log( nomeCompleto, nomecompleto)
+// console.log( nomeCompleto, nomecompleto)
 
-let _teste = "OK"
+// let _teste = "OK"
 
-let $teste =  "OK"
+// let $teste =  "OK"
 
-console.log($teste)
-console.log(_teste)
+// console.log($teste)
+// console.log(_teste)
 
-// 3 -  prompt
+// // 3 -  prompt
 
-//const age = prompt("Digite a sua idade")
+// //const age = prompt("Digite a sua idade")
 
-//console.log(`Você tem ${age} anos.`)
+// //console.log(`Você tem ${age} anos.`)
 
-// 4 - alert
+// // 4 - alert
 
-//alert("Testando");
+// //alert("Testando");
 
-//const z = 10
+// //const z = 10
 
-//alert(`O número é ${z}`)
+// //alert(`O número é ${z}`)
 
-// 5 - Math
+// // 5 - Math
 
-console.log(Math.max(5, 1, 2, 6 ,4 ,7, 10) ) 
+// console.log(Math.max(5, 1, 2, 6 ,4 ,7, 10) ) 
 
-console.log(Math.floor(5, 1, 2, 6 ,4 ,7, 10) ) 
+// console.log(Math.floor(5, 1, 2, 6 ,4 ,7, 10) ) 
 
-console.log(Math.ceil(5.14) ) 
+// console.log(Math.ceil(5.14) ) 
 
-// 6 - Console
-console.log("Teste!")
+// // 6 - Console
+// console.log("Teste!")
 
-console.error("erro!")
+// console.error("erro!")
 
-console.warn("Aviso!")
-
-
-// 7 - if 
-
-const m = 10
-
-if(m> 5) {
-
-    console.log("M é maior que 5")
-
-}
-
-const user = "João"
-
-if(user === "João"){
-
-    console.log("É João")
-
-}
-
-if(user === "Maria"){
-
-    console.log("Olá Maria")
-
-}
-
-console.log(user === "João", user === "Maria")
-
-// 8 - else 
-
-const loggedIn = false
-
-if(loggedIn) {
-    console.log("Está Autenticado!")
-}else{
-    console.log("Não está autenticado")
-}
+// console.warn("Aviso!")
 
 
+// // 7 - if 
 
-const q = 10
-const w = 25
+// const m = 10
 
-if(q > 5 && w > 20) {
-    console.log("Numeros mais altos")
-}else{
-    console.log("Os numeros não são mais altos")
-}
+// if(m> 5) {
+
+//     console.log("M é maior que 5")
+
+// }
+
+// const user = "João"
+
+// if(user === "João"){
+
+//     console.log("É João")
+
+// }
+
+// if(user === "Maria"){
+
+//     console.log("Olá Maria")
+
+// }
+
+// console.log(user === "João", user === "Maria")
+
+// // 8 - else 
+
+// const loggedIn = false
+
+// if(loggedIn) {
+//     console.log("Está Autenticado!")
+// }else{
+//     console.log("Não está autenticado")
+// }
+
+
+
+// const q = 10
+// const w = 25
+
+// if(q > 5 && w > 20) {
+//     console.log("Numeros mais altos")
+// }else{
+//     console.log("Os numeros não são mais altos")
+// }
 
 // 9 - else if
 
-if(1 > 2) {
-    console.log("Teste")
+// if(1 > 2) {
+//     console.log("Teste")
 
-} else if(2 > 3){
-    console.log("Teste 2")
+// } else if(2 > 3){
+//     console.log("Teste 2")
 
-}else if( 5 > 1 ){
-    console.log("Agora sim!")
+// }else if( 5 > 1 ){
+//     console.log("Agora sim!")
+// }
+
+// const userName = "Matheus"
+// const userAge = 31
+
+// if(userName === "José"){
+//     console.log("Bem vindo José")
+// }else if (userName === "Matheus" && userAge === 31){
+//     console.log("Olá Matheus, você tem 31 anos!")
+// }else{
+//     console.log("Nenhuma consição aceita!")
+// }
+
+// 10 - While
+
+let p = 0
+
+while (p < 5) {
+    console.log(`Repetindo ${p}`)
+    p = p + 1
 }
 
-const userName = "Matheus"
-const userAge = 31
+// loop infinito
+//  let x = 10
 
-if(userName === "José"){
-    console.log("Bem vindo José")
-}else if (userName === "Matheus" && userAge === 31){
-    console.log("Olá Matheus, você tem 31 anos!")
-}else{
-    console.log("Nenhuma consição aceita!")
-}
+//  while(x > 5){
+//     console.log(`imprimindo ${x}`)
+//  }
 
+// 11 - do while
+
+let o = 10
+
+do {
+    console.log(`valor de o: ${o}`)
+    o--
+}while( o > 1)
 
 
 
